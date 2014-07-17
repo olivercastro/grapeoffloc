96b9922c309b0ca41ebbd8f4a1fa8f412931787b
Oliver Castro <olivertamboboycastro@gmail.com>
2014-07-16 23:35:58 -0700

    Creation of the Advance Filter Extension

---
+++ app/code/local/Homebase/Advancefilter/Block/Index.php
@@ -0,0 +1,8 @@
+<?php   
+class Homebase_Advancefilter_Block_Index extends Mage_Core_Block_Template{   
+
+
+
+
+
+}
---
+++ app/code/local/Homebase/Advancefilter/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Homebase_Advancefilter_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
